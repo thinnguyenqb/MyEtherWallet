5147cdda9333ac276f69382b9e4e590ad6c3c17d
EricNguyen99 <vanthin7111999@gmail.com>
2022-05-01 23:59:00 +0700

    add: transactions in api

---
+++ wallet-coin/app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const compression = require("compression")
+const app = express()
+
+app.use(express.json())
+app.use(compression())
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/", (req, res) => {
+  return res.send("hihi")
+})
+
+app.listen(PORT, () => {
+  console.log("Server is listen on port : " + PORT);
+})
---
+++ wallet-coin/lib/blockchain/block.js
@@ -0,0 +1,35 @@
+const R = require('ramda');
+const CryptoUtil = require('../util/CryptoUtil')
+
+class Block {
+  toHash() {
+      
+      return CryptoUtil.hash(this.index + this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce);
+  }
+
+  getDifficulty() {
+      
+      return parseInt(this.hash.substring(0, 14), 16);
+  }
+
+  static get genesis() {
+      return Block.fromJson(Config.genesisBlock);
+  }
+
+  static fromJson(data) {
+      let block = new Block();
+      R.forEachObjIndexed((value, key) => {
+          if (key == 'transactions' && value) {
+              block[key] = Transactions.fromJson(value);
+          } else {
+              block[key] = value;
+          }
+      }, data);
+
+      block.hash = block.toHash();
+      return block;
+  }
+
+}
+
+module.exports = Block;
