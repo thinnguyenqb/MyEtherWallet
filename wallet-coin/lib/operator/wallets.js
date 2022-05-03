9b9f5c7b407838a082f8c1ed65092cc60ebe4974
EricNguyen99 <vanthin7111999@gmail.com>
2022-05-03 23:14:20 +0700

    add: utilities

---
+++ wallet-coin/lib/operator/wallets.js
@@ -0,0 +1,14 @@
+const Wallet = require('./wallet');
+const R = require('ramda');
+
+class Wallets extends Array {
+  static fromJson(data) {
+    let wallets = new Wallets();
+    R.forEach((wallet) => {
+      wallets.push(Wallet.fromJson(wallet));
+    }, data);
+    return wallets;
+  }
+}
+
+module.exports = Wallets;
